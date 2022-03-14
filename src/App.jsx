0688d87d87ce26413ef52c9d1b87b8e58af95a2f
andres-condezo <ra.condezomonge@gmail.com>
2022-03-14 14:01:05 -0500

    Delete default files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+
+const App = () => {
+  return (
+    <>
+    <Header />
+      <main>
+        <h1>Welcome</h1>
+      </main>
+    <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Footer = () => {
+  return(
+    <div>
+      <footer>Footer</footer>
+    </div>
+    )
+}
+
+export default Footer
