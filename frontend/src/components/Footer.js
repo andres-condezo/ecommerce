339a35b5c9155cc580d358f78413686d480d53f8
rennleon <rennleonz@gmail.com>
2022-03-16 21:03:58 -0500

    Add Bootstrap styling

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <footer>My Footer</footer>      
+    </div>
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <header>My Header</header>
+    </div>
+  )
+}
+
+export default Header
