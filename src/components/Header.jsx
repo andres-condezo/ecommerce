9a63b88c5870b21ab2341b64ecfa738e005acaed
andres-condezo <ra.condezomonge@gmail.com>
2022-03-18 00:57:40 -0500

    Add item title and item description

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,20 +6,23 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Nav
-            activeKey="/home"
-            className="mr-auto"
-          >
-            <Nav.Item>
-              <Nav.Link href="/home">ProShop</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="cart"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="login"><i className="fas fa-user"></i>Login</Nav.Link>
-            </Nav.Item>
-          </Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav
+              activeKey="/home"
+              className="mr-auto"
+            >
+              <Nav.Item>
+                <Nav.Link href="/home">ProShop</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="cart"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="login"><i className="fas fa-user"></i>Login</Nav.Link>
+              </Nav.Item>
+            </Nav>
+            </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,5 +1,6 @@
 import { Row, Col } from 'react-bootstrap';
 import products from '../products';
+import Product from '../components/Product';
 
 function HomeScreen(){
   return(
@@ -7,8 +8,8 @@ function HomeScreen(){
       <h1>Latest Products</h1>
     <Row>
     {products.map(product => (
-      <Col sm={12} md={6} Lg={4} xL={3}>
-        <h3>{product.name}</h3>
+      <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+        <Product product={product} />
       </Col>
     ))}      
     </Row>
