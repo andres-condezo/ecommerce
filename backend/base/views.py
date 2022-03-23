b97439a7f3eb97a755584ec0b7beb849c9050067
rennleon <rennleonz@gmail.com>
2022-03-23 02:03:43 -0500

    Add 'products/' route

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .products import products
 
 # Create your views here.
 def getRoutes(request):
@@ -18,3 +19,7 @@ def getRoutes(request):
     '/api/products/<update>/<id>/',
   ]
   return JsonResponse(routes, safe=False)
+
+
+def getProducts(request):
+  return JsonResponse(products, safe=False)
