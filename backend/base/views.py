06b90f306179ceca79218a2ffa371fac33c66136
rennleon <rennleonz@gmail.com>
2022-03-23 02:14:23 -0500

    Setup get product route

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from .products import products
 
 # Create your views here.
+
+@api_view(['GET'])
 def getRoutes(request):
   routes = [
     '/api/products/',
@@ -18,8 +23,20 @@ def getRoutes(request):
     '/api/products/delete/<id>/',
     '/api/products/<update>/<id>/',
   ]
-  return JsonResponse(routes, safe=False)
+  # return JsonResponse(routes, safe=False)
+  return Response(routes)
 
 
+@api_view(['GET'])
 def getProducts(request):
-  return JsonResponse(products, safe=False)
+  # return JsonResponse(products, safe=False)
+  return Response(products)
+
+
+@api_view(['GET'])
+def getProduct(request, pk):
+  for prod in products:
+    if prod['_id'] == pk:
+      return Response(prod)
+
+  return None
