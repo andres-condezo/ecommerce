6608e49a238a88ddedab70d6ebf100938c1fd588
rennleon <renleonz@gmail.com>
2022-03-27 18:02:21 -0500

    Update api product endpoint to use Product model

--- backend/base/views.py
+++ backend/base/views.py
@@ -31,7 +31,6 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProducts(request):
-  # return JsonResponse(products, safe=False)
   products = Product.objects.all()
   serializer = ProductSerializer(products, many=True)
   return Response(serializer.data)
@@ -39,8 +38,6 @@ def getProducts(request):
 
 @api_view(['GET'])
 def getProduct(request, pk):
-  for prod in products:
-    if prod['_id'] == pk:
-      return Response(prod)
-
-  return Response(None)
+  product = Product.objects.get(_id=pk)
+  serializer = ProductSerializer(product, many=False)
+  return Response(serializer.data)
