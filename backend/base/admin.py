c1f12785fee8fe43b06dfb323121777cf3ee54b4
rennleon <renleonz@gmail.com>
2022-03-27 17:18:24 -0500

    Create models and apply migrations

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Product
+from .models import Review
+from .models import Order
+from .models import OrderItem
+from .models import ShippingAddress
 
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(Review)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
