c7c41767ccf21d4e111e1d444a7fd533127df2e9
rennleon <renleonz@gmail.com>
2022-03-31 08:21:02 -0500

    Create and setup productListReduucers

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,11 @@ import { createStore, combineReducers, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
-const reducer = combineReducers({})
+import { productListReducers } from './reducers/productReducers'
+
+const reducer = combineReducers({
+    productList: productListReducers
+})
 
 const initialState = {}
 
