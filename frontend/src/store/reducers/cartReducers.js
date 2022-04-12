4de67969260e4fb6ca25dccac56f7bc97c86bcfc
rennleon <renleonz@gmail.com>
2022-04-12 03:05:46 -0500

    Create CART_REMOVE_ITEM action and reducer

--- frontend/src/store/reducers/cartReducers.js
+++ frontend/src/store/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 export const cartReducers = (state = { cartItems: [] }, action) => {
   switch (action.type) {
@@ -18,6 +18,13 @@ export const cartReducers = (state = { cartItems: [] }, action) => {
       return {
         ...state,
         cartItems: [...state.cartItems, item],
+      };
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (item) => item.product !== action.payload
+        ),
       };
     default:
       return state;
