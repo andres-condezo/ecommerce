9e5d1f7283ef0ef84f992155f703274e5a78f9fc
rennleon <renleonz@gmail.com>
2022-04-12 02:06:19 -0500

    Fix typo 'Add and s to complete cartItems'

--- frontend/src/store/reducers/cartReducers.js
+++ frontend/src/store/reducers/cartReducers.js
@@ -9,7 +9,7 @@ export const cartReducers = (state = { cartItems: [] }, action) => {
       if (existItem) {
         return {
           ...state,
-          cartItem: state.cartItems.map((x) =>
+          cartItems: state.cartItems.map((x) =>
             x.product === existItem.product ? item : x
           ),
         };
