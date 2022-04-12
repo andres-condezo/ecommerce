24f1bcc8db612a460c431e0ea5f320dd9c7294fe
rennleon <renleonz@gmail.com>
2022-04-12 02:40:54 -0500

    Add routes for CartScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Container } from "react-bootstrap";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 
@@ -9,12 +10,14 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-      
+
       <main className="py-3">
         <Container>
           <Routes>
             <Route path="/" element={<HomeScreen />} exact />
             <Route path="/product/:id" element={<ProductScreen />} />
+            <Route path="/cart/:id" element={<CartScreen />} />
+            <Route path="/cart" element={<CartScreen />} />
           </Routes>
         </Container>
       </main>
