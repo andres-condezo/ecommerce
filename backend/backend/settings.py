6a29f90b9e800c91434e0fe7853e576090bd6ee4
rennleon <renleonz@gmail.com>
2022-04-19 22:55:15 -0500

    Add REST_FRAMEWORK settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -44,6 +44,12 @@ INSTALLED_APPS = [
     'base.apps.BaseConfig'
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
 
