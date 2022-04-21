4c0b9eb4dca13bc9cb3a09785b7787504a8ba962
rennleon <renleonz@gmail.com>
2022-04-20 23:31:50 -0500

    Create UserSerializer

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,6 +1,34 @@
+from copyreg import constructor
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import *
+from .models import Product
+
+
+class UserSerializer(serializers.ModelSerializer):
+    _id = serializers.SerializerMethodField(read_only=True)
+    name = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin']
+
+    def get__id(self, obj):
+        return obj.id
+
+    def get_isAdmin(self, obj):
+        return obj.is_staff
+
+    def get_name(self, obj):
+        name = ''
+        if hasattr(obj, 'first_name'):
+            name = obj.first_name
+        if name == '' and hasattr(obj, 'email'):
+            name = obj.email
+        
+        print(dir(obj))
+
+        return name
 
 
 class ProductSerializer(serializers.ModelSerializer):
