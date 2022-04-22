cadad386993c1c03856d16807c6ba3702f3d5b3b
rennleon <renleonz@gmail.com>
2022-04-21 23:01:13 -0500

    Create new enpoint to register users

--- backend/base/views.py
+++ backend/base/views.py
@@ -4,12 +4,15 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 
 from .models import Product
 from .serializers import (ProductSerializer, UserSerializer, UserSerializerWithToken)
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+from rest_framework import status
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -68,6 +71,24 @@ def getUsers(request):
   users = User.objects.all()
   serializer = UserSerializer(users, many=True)
   return Response(serializer.data)
+
+@api_view(['POST'])
+def registerUser(request):
+  data = request.data
+
+  try:
+    user = User.objects.create(
+      first_name=data['name'],
+      username=data['email'],
+      email=data['email'],
+      password=make_password(data['password'])
+    )
+
+    serializer = UserSerializerWithToken(user, many=False)
+    return Response(serializer.data)
+  except:
+    message = {'datail': 'User with this email already exists'}
+    return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
