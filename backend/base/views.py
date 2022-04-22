6ac4389b1cfc2797ec1341aa22919d1d2bc525ca
rennleon <renleonz@gmail.com>
2022-04-21 22:02:45 -0500

    Add new endpoint for users and add route protection

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,6 +1,9 @@
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
+
+from django.contrib.auth.models import User
 
 from .models import Product
 from .serializers import (ProductSerializer, UserSerializer, UserSerializerWithToken)
@@ -53,9 +56,17 @@ def getRoutes(request):
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def geUserProfile(request):
   user = request.user
   serializer = UserSerializer(user, many=False)
+  return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+  users = User.objects.all()
+  serializer = UserSerializer(users, many=True)
   return Response(serializer.data)
 
 
