32f54d4eb59b986464bf316115451deaa0ebb637
rennleon <renleonz@gmail.com>
2022-04-25 23:14:16 -0500

    Create user login actions

---
+++ frontend/src/store/actions/userActions.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+} from "../constants/userConstants";
+
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({ type: USER_LOGIN_REQUEST });
+
+    const loginData = {
+      username: email,
+      password: password,
+    };
+
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
+
+    const { data } = await axios.post("/api/users/login/", loginData, config);
+
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload: error.response.data.detail
+        ? error.response.data.detail
+        : error.detail,
+    });
+  }
+};
