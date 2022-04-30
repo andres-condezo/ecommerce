a3b45cc1903b47616509431333dc0b13caddfb6b
rennleon <renleonz@gmail.com>
2022-04-30 11:17:25 -0500

    Create FormContainer component

---
+++ frontend/src/components/FormContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+export const FormContainer = ({ children }) => {
+  return (
+    <Container>
+      <Row className="justify-content-md-center">
+        <Col xs={12} md={6}>
+          {children}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
