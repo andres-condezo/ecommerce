23aa67bfe6a3c6c8ca04ee0212c3975f4109084c
rennleon <renleonz@gmail.com>
2022-04-30 12:30:13 -0500

    Create user action for logout

--- frontend/src/store/actions/userActions.js
+++ frontend/src/store/actions/userActions.js
@@ -3,6 +3,7 @@ import {
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
 } from "../constants/userConstants";
 
 export const login = (email, password) => async (dispatch) => {
@@ -33,4 +34,9 @@ export const login = (email, password) => async (dispatch) => {
         : error.detail,
     });
   }
+};
+
+export const logout = () => async (dispatch) => {
+  localStorage.removeItem("userInfo");
+  dispatch({ type: USER_LOGOUT });
 };
