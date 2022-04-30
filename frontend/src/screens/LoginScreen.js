bd1f0e234ab47becf44fa7860583731523202cde
rennleon <renleonz@gmail.com>
2022-04-30 11:13:13 -0500

    Create LoginScreen

---
+++ frontend/src/screens/LoginScreen.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+export const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return <div>LoginScreen</div>;
+};
