4cc21ae3ff7b78b319636e18dce416a5cd0fa6f3
rennleon <renleonz@gmail.com>
2022-05-03 08:56:03 -0500

    Create user register action

--- frontend/src/store/actions/userActions.js
+++ frontend/src/store/actions/userActions.js
@@ -4,6 +4,9 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGOUT,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
 } from "../constants/userConstants";
 
 export const login = (email, password) => async (dispatch) => {
@@ -39,4 +42,35 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => async (dispatch) => {
   localStorage.removeItem("userInfo");
   dispatch({ type: USER_LOGOUT });
+};
+
+export const register = (name, email, password) => async (dispatch) => {
+  try {
+    dispatch({ type: USER_REGISTER_REQUEST });
+
+    const userData = {
+      name,
+      username: email,
+      password: password,
+    };
+
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
+
+    const { data } = await axios.post("/api/users/register/", userData, config);
+
+    dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
+
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload: error.response.data.detail
+        ? error.response.data.detail
+        : error.detail,
+    });
+  }
 };
