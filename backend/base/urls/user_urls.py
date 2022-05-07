1e8dd5083a9559fac859ab89ab4a864eaf968a01
rennleon <renleonz@gmail.com>
2022-05-07 10:41:34 -0500

    Create an endpoint to update user profile data

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -3,7 +3,11 @@ from base.views import user_views as views
 
 urlpatterns = [
   path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+
   path('register/', views.registerUser, name='user_register'),
+
   path('profile/', views.getUserProfile, name='users_profile'),
+  path('profile/update/', views.updateUserProfile, name='users_profile_update'),
+
   path('', views.getUsers, name='users'),
 ]
