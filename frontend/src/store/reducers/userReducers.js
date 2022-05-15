69571b3c6a98877dda4df36321a7c49f9e2bd06b
rennleon <renleonz@gmail.com>
2022-05-15 13:45:31 -0500

    Create reducer for user update profile

--- frontend/src/store/reducers/userReducers.js
+++ frontend/src/store/reducers/userReducers.js
@@ -9,6 +9,10 @@ import {
   USER_REGISTER_FAIL,
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_SUCCESS,
+  USER_UPDATE_PROFILE_RESET,
+  USER_UPDATE_PROFILE_FAIL,
 } from "../constants/userConstants";
 
 export const userLoginReducer = (state = {}, action) => {
@@ -56,6 +60,25 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
 
     case USER_DETAILS_FAIL:
       return { loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
+
+export const userUpdateProfileReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_UPDATE_PROFILE_REQUEST:
+      return { loading: true };
+
+    case USER_UPDATE_PROFILE_SUCCESS:
+      return { loading: false, success: true, userInfo: action.payload };
+
+    case USER_UPDATE_PROFILE_FAIL:
+      return { loading: false, error: action.payload };
+
+    case USER_UPDATE_PROFILE_RESET:
+      return {};
 
     default:
       return state;
