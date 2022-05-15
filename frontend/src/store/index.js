b5f2d1ce497b00f644d709833d4eead9983527cb
rennleon <renleonz@gmail.com>
2022-05-15 13:47:12 -0500

    Include user update profile reducer into combine reducers

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -12,6 +12,7 @@ import {
   userDetailsReducer,
   userLoginReducer,
   userRegisterReducer,
+  userUpdateProfileReducer,
 } from "./reducers/userReducers";
 
 const reducer = combineReducers({
@@ -21,6 +22,7 @@ const reducer = combineReducers({
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
 });
 
 const storedCartItems = localStorage.getItem("cartItems");
