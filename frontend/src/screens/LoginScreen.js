6a0e286f808bf64374c7446c7bcd25dede032cd8
rennleon <renleonz@gmail.com>
2022-05-18 14:26:21 -0500

    Fix redirection

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -23,7 +23,7 @@ export const LoginScreen = () => {
 
   useEffect(() => {
     if (userInfo) {
-      navigate(redirect);
+      navigate(`/${redirect}`);
     }
   }, [navigate, userInfo, redirect]);
 
