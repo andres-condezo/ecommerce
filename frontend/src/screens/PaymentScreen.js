1066a1213ab774d74864cda8fc6e93f9475bb136
rennleon <renleonz@gmail.com>
2022-05-25 15:05:32 -0500

    Create PaymentScreen.js

---
+++ frontend/src/screens/PaymentScreen.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { CheckoutSteps } from "../components/CheckoutSteps";
+import { FormContainer } from "../components/FormContainer";
+
+export const PaymentScreen = () => {
+  const [paymentMethod, setPaymentMethod] = useState("Paypal");
+
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
+  if (!shippingAddress.address) {
+    navigate("/shipping");
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    // dispatch(savePaymentMethod(paymentMethod))
+    navigate("/placeorder");
+  };
+
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 step3 />
+
+      <Form onSUmbmit={submitHandler}>
+        <Form.Group>
+          <Form.Label as="legend">Select Method</Form.Label>
+
+          <Col>
+            <Form.Check
+              type="radio"
+              label="Paypal or Credit Card"
+              id="paypal"
+              name="paymentMethod"
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check>
+          </Col>
+        </Form.Group>
+
+        <Button type="submit" variant="primary">
+          Continue
+        </Button>
+      </Form>
+    </FormContainer>
+  );
+};
