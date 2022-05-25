755ece515f6dcf780bfdb22a19136fa869eb126e
rennleon <renleonz@gmail.com>
2022-05-25 15:06:47 -0500

    Add missing imports

--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form } from "react-bootstrap";
+import { Button, Col, Form } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { CheckoutSteps } from "../components/CheckoutSteps";
