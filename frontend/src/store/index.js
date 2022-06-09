0f0a892b55f46ad214ba93049d036f4585f4c61d
rennleon <renleonz@gmail.com>
2022-06-08 20:39:33 -0500

    Create order constants and reducer

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -14,6 +14,7 @@ import {
   userRegisterReducer,
   userUpdateProfileReducer,
 } from "./reducers/userReducers";
+import { orderCreateReducer } from "./reducers/orderReducers";
 
 const reducer = combineReducers({
   productList: productListReducers,
@@ -23,6 +24,7 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
+  orderCreate: orderCreateReducer,
 });
 
 const storedCartItems = localStorage.getItem("cartItems");
