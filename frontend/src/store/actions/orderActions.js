14c23df0c8f4aee9842ba56fc2e6773e7378c36d
rennleon <renleonz@gmail.com>
2022-06-08 20:50:19 -0500

    Create action for order

---
+++ frontend/src/store/actions/orderActions.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS } from "../constants/orderConstants";
+
+export const createOrder = (order) => (dispatch, getState) => {
+  try {
+    dispatch({ type: ORDER_CREATE_REQUEST });
+
+    const {userLogin: {userInfo}} = getState();
+
+    const config = {
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`
+      }
+    }
+
+    const { data } = await axios.post(
+      '/api/orders/add/',
+      order,
+      config
+    );
+
+    dispatch({
+      type: ORDER_CREATE_SUCCESS,
+      payload: data
+    })
+  } catch (error) {
+    dispatch({
+      type: ORDER_CREATE_FAIL,
+      payload: error.response && error.response.data.detail
+        ? error.response.data.detail
+        : error.message
+    })
+  }
+};
