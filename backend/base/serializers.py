3bb2619874adc92363194609db27fc9949c266ce
Roberto Andres Condezo Monge <ra.condezomonge@gmail.com>
2022-06-08 23:51:54 -0500

    Create UserSerializerWithToken method

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 from .models import Product
 
 
@@ -26,6 +27,16 @@ class UserSerializer(serializers.ModelSerializer):
 
         return name
 
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin', 'token']
+
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token)
 
 class ProductSerializer(serializers.ModelSerializer):
     '''ProductSerializer model'''
--- backend/base/views.py
+++ backend/base/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 
 from .models import Product
 # from .products import products
-from .serializers import ProductSerializer, UserSerializer
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 # Create your views here.
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
@@ -15,8 +15,9 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
 
-        data['username'] = self.user.username
-        data['email'] = self.user.email
+        serializer = UserSerializerWithToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
 
         return data
 
