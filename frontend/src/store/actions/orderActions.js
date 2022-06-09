212ffa4eedb865439738dab066963ec01f59472e
rennleon <renleonz@gmail.com>
2022-06-08 20:56:56 -0500

    Create git stat

--- frontend/src/store/actions/orderActions.js
+++ frontend/src/store/actions/orderActions.js
@@ -1,18 +1,24 @@
 import axios from "axios";
-import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS } from "../constants/orderConstants";
+import {
+  ORDER_CREATE_FAIL,
+  ORDER_CREATE_REQUEST,
+  ORDER_CREATE_SUCCESS,
+} from "../constants/orderConstants";
 
 export const createOrder = (order) => (dispatch, getState) => {
   try {
     dispatch({ type: ORDER_CREATE_REQUEST });
 
-    const {userLogin: {userInfo}} = getState();
+    const {
+      userLogin: { userInfo },
+    } = getState();
 
     const config = {
       headers: {
-        'Content-type': 'application/json',
-        Authorization: `Bearer ${userInfo.token}`
-      }
-    }
+        "Content-type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
 
     const { data } = await axios.post(
       '/api/orders/add/',
@@ -22,14 +28,15 @@ export const createOrder = (order) => (dispatch, getState) => {
 
     dispatch({
       type: ORDER_CREATE_SUCCESS,
-      payload: data
-    })
+      payload: data,
+    });
   } catch (error) {
     dispatch({
       type: ORDER_CREATE_FAIL,
-      payload: error.response && error.response.data.detail
-        ? error.response.data.detail
-        : error.message
-    })
+      payload:
+        error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    });
   }
 };
