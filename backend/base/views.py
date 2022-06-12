8e59c550ff60ab96511d3ea70668d5b744da5147
Roberto Andres Condezo Monge <ra.condezomonge@gmail.com>
2022-06-11 23:02:26 -0500

    [Add] [Protect routes]
     Changes to be committed:
    	modified:   backend/base/__pycache__/serializers.cpython-310.pyc
    	modified:   backend/base/__pycache__/urls.cpython-310.pyc
    	modified:   backend/base/__pycache__/views.cpython-310.pyc
    	modified:   backend/base/urls.py
    	modified:   backend/base/views.py

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
+
+from django.contrib.auth.models import User
 
 from .models import Product
 # from .products import products
@@ -44,20 +47,25 @@ def getRoutes(request):
     ]
     return Response(routes)
 
-
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
     return Response(serializer.data)
 
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProducts(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
-
 
 @api_view(['GET'])
 def getProduct(request, pk):
