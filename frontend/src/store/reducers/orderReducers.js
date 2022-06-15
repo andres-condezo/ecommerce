faf614cb45eb9045164486a4166a6eccaaa9ecd3
rennleon <renleonz@gmail.com>
2022-06-15 15:59:32 -0500

    Create new reducer for order pay

--- frontend/src/store/reducers/orderReducers.js
+++ frontend/src/store/reducers/orderReducers.js
@@ -6,6 +6,9 @@ import {
   ORDER_DETAILS_FAIL,
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
+  ORDER_PAY_FAIL,
+  ORDER_PAY_REQUEST,
+  ORDER_PAY_SUCCESS,
 } from "../constants/orderConstants";
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -23,7 +26,7 @@ export const orderCreateReducer = (state = {}, action) => {
     case ORDER_CREATE_FAIL:
       return {
         loading: false,
-        payload: action.payload,
+        error: action.payload,
       };
 
     case ORDER_CREATE_RESET:
@@ -52,8 +55,33 @@ export const orderDetailsReducer = (
     case ORDER_DETAILS_FAIL:
       return {
         loading: false,
-        payload: action.payload,
+        error: action.payload,
       };
+
+    default:
+      return state;
+  }
+};
+
+export const orderPayReducer = (state = {}, action) => {
+  switch (action.type) {
+    case ORDER_PAY_REQUEST:
+      return { loading: true };
+
+    case ORDER_PAY_SUCCESS:
+      return {
+        loading: false,
+        success: true,
+      };
+
+    case ORDER_PAY_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+
+    case ORDER_PAY_RESET:
+      return {};
 
     default:
       return state;
