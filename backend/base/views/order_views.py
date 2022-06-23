3ead599e43501ca66ecca5e08bf00fbb142c9be8
rennleon <renleonz@gmail.com>
2022-06-22 20:55:44 -0500

    Crete view for my orders

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -74,6 +74,14 @@ def getOrderById(request, pk):
   except:
     return Response({'detail': 'Order does not exist'}, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getMyOrders(request):
+  user = request.user
+  orders = user.order_set.all()
+  serializer = OrderSerializer(orders, many=True)
+  return Response(serializer.data)
+
 
 @api_view(['PUT'])
 @permission_classes([IsAuthenticated])
