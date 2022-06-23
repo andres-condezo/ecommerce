1870fc74e7b57e4b5dedca953cf6e3c1a034d2be
rennleon <renleonz@gmail.com>
2022-06-22 21:07:53 -0500

    Update user actions to reset order on logout

--- frontend/src/store/actions/userActions.js
+++ frontend/src/store/actions/userActions.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { ORDER_LIST_MY_RESET } from "../constants/orderConstants";
 import {
   USER_DETAILS_FAIL,
   USER_DETAILS_REQUEST,
@@ -50,6 +51,7 @@ export const logout = () => async (dispatch) => {
   localStorage.removeItem("userInfo");
   dispatch({ type: USER_LOGOUT });
   dispatch({ type: USER_DETAILS_RESET });
+  dispatch({ type: ORDER_LIST_MY_RESET });
 };
 
 export const register = (name, email, password) => async (dispatch) => {
