8cdfdd72307345cc8cd2b1bdf703f734d36830cb
rennleon <renleonz@gmail.com>
2022-06-22 20:59:56 -0500

    Create reducer for my orders

--- frontend/src/store/reducers/orderReducers.js
+++ frontend/src/store/reducers/orderReducers.js
@@ -6,6 +6,10 @@ import {
   ORDER_DETAILS_FAIL,
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
+  ORDER_LIST_MY_FAIL,
+  ORDER_LIST_MY_REQUEST,
+  ORDER_LIST_MY_RESET,
+  ORDER_LIST_MY_SUCCESS,
   ORDER_PAY_FAIL,
   ORDER_PAY_REQUEST,
   ORDER_PAY_RESET,
@@ -83,6 +87,33 @@ export const orderPayReducer = (state = {}, action) => {
 
     case ORDER_PAY_RESET:
       return {};
+
+    default:
+      return state;
+  }
+};
+
+export const orderListMyReducer = (state = {}, action) => {
+  switch (action.type) {
+    case ORDER_LIST_MY_REQUEST:
+      return { loading: true };
+
+    case ORDER_LIST_MY_SUCCESS:
+      return {
+        loading: false,
+        orders: action.payload,
+      };
+
+    case ORDER_LIST_MY_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+
+    case ORDER_LIST_MY_RESET:
+      return {
+        orders: [],
+      };
 
     default:
       return state;
