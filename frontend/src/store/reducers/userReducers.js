965a5da1189c2c924350f1b3740f818c571dee3c
rennleon <renleonz@gmail.com>
2022-06-25 19:08:56 -0500

    Create reducer for user list

--- frontend/src/store/reducers/userReducers.js
+++ frontend/src/store/reducers/userReducers.js
@@ -14,6 +14,10 @@ import {
   USER_UPDATE_PROFILE_RESET,
   USER_UPDATE_PROFILE_FAIL,
   USER_DETAILS_RESET,
+  USER_LIST_REQUEST,
+  USER_LIST_SUCCESS,
+  USER_LIST_FAIL,
+  USER_LIST_RESET,
 } from "../constants/userConstants";
 
 export const userLoginReducer = (state = {}, action) => {
@@ -83,6 +87,25 @@ export const userUpdateProfileReducer = (state = {}, action) => {
 
     case USER_UPDATE_PROFILE_RESET:
       return {};
+
+    default:
+      return state;
+  }
+};
+
+export const userListReducer = (state = { users: [] }, action) => {
+  switch (action.type) {
+    case USER_LIST_REQUEST:
+      return { loading: true };
+
+    case USER_LIST_SUCCESS:
+      return { loading: false, users: action.payload };
+
+    case USER_LIST_FAIL:
+      return { loading: false, error: action.payload };
+
+    case USER_LIST_RESET:
+      return { users: [] };
 
     default:
       return state;
