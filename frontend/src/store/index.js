a3895db74247d685235b9d87103b8868b759ee2a
rennleon <renleonz@gmail.com>
2022-06-25 19:10:28 -0500

    Add user lists reducer to reducers

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -10,6 +10,7 @@ import {
 import { cartReducers } from "./reducers/cartReducers";
 import {
   userDetailsReducer,
+  userListReducer,
   userLoginReducer,
   userRegisterReducer,
   userUpdateProfileReducer,
@@ -29,6 +30,7 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
+  userList: userListReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
