5fdff25c47f222e5bbd82332775192046dafd74a
rennleon <renleonz@gmail.com>
2022-06-27 21:56:08 -0500

    Create action for user update

--- frontend/src/store/actions/userActions.js
+++ frontend/src/store/actions/userActions.js
@@ -19,9 +19,12 @@ import {
   USER_REGISTER_FAIL,
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
+  USER_UPDATE_FAIL,
   USER_UPDATE_PROFILE_FAIL,
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
+  USER_UPDATE_REQUEST,
+  USER_UPDATE_SUCCESS,
 } from "../constants/userConstants";
 
 export const login = (email, password) => async (dispatch) => {
@@ -200,12 +203,41 @@ export const deleteUser = (id) => async (dispatch, getState) => {
       },
     };
 
-    const { data } = await axios.delete(`/api/users/delete/${id}`, config);
+    const { data } = await axios.delete(`/api/users/delete/${id}/`, config);
 
     dispatch({ type: USER_DELETE_SUCCESS, payload: data });
   } catch (error) {
     dispatch({
       type: USER_DELETE_FAIL,
+      payload: error.response.data.detail
+        ? error.response.data.detail
+        : error.detail,
+    });
+  }
+};
+
+export const updateUser = (user) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: USER_UPDATE_REQUEST });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.put(`/api/users/update/${id}/`, user, config);
+
+    dispatch({ type: USER_UPDATE_SUCCESS });
+    dispatch({ type: USER_DETAILS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: USER_UPDATE_FAIL,
       payload: error.response.data.detail
         ? error.response.data.detail
         : error.detail,
