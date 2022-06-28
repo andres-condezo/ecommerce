b9850dfe68e3bc8a2aa51977918789c6f972adf3
rennleon <renleonz@gmail.com>
2022-06-27 20:43:53 -0500

    Create an endpoint to delete users

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
   path('profile/update/', views.updateUserProfile, name='users_profile_update'),
 
   path('', views.getUsers, name='users'),
+
+  path('delete/<str:pk>/', views.deleteUser, name='user-delete'),
 ]
