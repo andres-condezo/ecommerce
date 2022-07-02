916a05cf35cf14d849785e3fd9ea3c30c311548b
rennleon <renleonz@gmail.com>
2022-07-01 21:35:08 -0500

    Create view to update product

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -40,6 +40,24 @@ def createProduct(request):
   return Response(serializer.data)
 
 
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateProduct(request, pk):
+  data = request.data
+  product = Product.objects.get(_id=pk)
+
+  product.name = data['name']
+  product.price = data['price']
+  product.brand = data['brand']
+  product.countInStock = data['countInStock']
+  product.category = data['category']
+  product.description = data['description']
+  product.save()
+  
+  serializer = ProductSerializer(product, many=False)
+  return Response(serializer.data)
+
+
 @api_view(['DELETE'])
 @permission_classes([IsAdminUser])
 def deleteProduct(request, pk):
