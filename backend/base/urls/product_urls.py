f57019e699133343b8bcd78573689847ed6df587
rennleon <renleonz@gmail.com>
2022-07-01 21:23:34 -0500

    Create endpoint for product create

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -4,5 +4,6 @@ from base.views import product_views as views
 urlpatterns = [
   path('', views.getProducts, name='products'),
   path('<str:pk>/', views.getProduct, name='product'),
+  path('create/', views.createProduct, name='product-create'),
   path('delete/<str:pk>/', views.deleteProduct, name='product-delete'),
 ]
