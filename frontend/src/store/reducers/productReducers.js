5871a998bf028d8d85c1e2830f524578d05e4aa8
rennleon <renleonz@gmail.com>
2022-07-01 21:02:55 -0500

    Create reducer for product delete

--- frontend/src/store/reducers/productReducers.js
+++ frontend/src/store/reducers/productReducers.js
@@ -5,6 +5,9 @@ import {
   PRODUCT_DETAILS_FAIL,
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DELETE_REQUEST,
+  PRODUCT_DELETE_SUCCESS,
+  PRODUCT_DELETE_FAIL,
 } from "../constants/productConstants";
 
 export const productListReducers = (state = { products: [] }, action) => {
@@ -30,6 +33,22 @@ export const productDetailsReducers = (
     case PRODUCT_DETAILS_SUCCESS:
       return { loading: false, product: action.payload };
     case PRODUCT_DETAILS_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const productDeleteReducer = (
+  state = { product: { reviews: [] } },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_DELETE_REQUEST:
+      return { loading: true };
+    case PRODUCT_DELETE_SUCCESS:
+      return { loading: false, success: true };
+    case PRODUCT_DELETE_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
