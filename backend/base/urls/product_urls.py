78c3c8ee2bfb36e5a5033179caf7b473a6bd78f0
rennleon <renleonz@gmail.com>
2022-07-01 21:36:52 -0500

    Create endpoint for product update

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
   path('', views.getProducts, name='products'),
   path('<str:pk>/', views.getProduct, name='product'),
   path('create/', views.createProduct, name='product-create'),
+  path('update/<str:pk>/', views.updateProduct, name='product-update'),
   path('delete/<str:pk>/', views.deleteProduct, name='product-delete'),
 ]
