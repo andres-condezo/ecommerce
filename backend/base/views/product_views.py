fd76a6e7729930815c761acf83a88d340e58951d
rennleon <renleonz@gmail.com>
2022-07-01 20:58:05 -0500

    Create view for product deletion

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -1,4 +1,5 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser
 
 from rest_framework.response import Response
 
@@ -18,3 +19,11 @@ def getProduct(request, pk):
   product = Product.objects.get(_id=pk)
   serializer = ProductSerializer(product, many=False)
   return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteProduct(request, pk):
+  product = Product.objects.get(_id=pk)
+  product.delete()
+  return Response('Product deleted')
