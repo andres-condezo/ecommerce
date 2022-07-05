7b3de7592387ce9ba21c2ec4afb86c37d558b9b9
rennleon <renleonz@gmail.com>
2022-07-05 09:23:16 -0500

    Create view to list all orders

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -83,6 +83,14 @@ def getMyOrders(request):
   return Response(serializer.data)
 
 
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getOrders(request):
+  orders = Order.objects.all()
+  serializer = OrderSerializer(orders, many=True)
+  return Response(serializer.data)
+
+
 @api_view(['PUT'])
 @permission_classes([IsAuthenticated])
 def updateOrderToPay(request, pk):
