c1998f2d32d01bd351a15e87e067abdb7e84a5de
rennleon <renleonz@gmail.com>
2022-07-05 09:24:24 -0500

    Create endpoint to list all orders

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from base.views import order_views as views
 
 urlpatterns = [
+  path('', views.getOrders, name='all-orders'),
+
   path('add/', views.addOrderItems, name='orders-add'),
   path('myorders/', views.getMyOrders, name='my-orders'),
 
