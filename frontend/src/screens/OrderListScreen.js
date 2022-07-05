2cf9ca5811944fe75e6f93f624e96e8060d1ec5e
rennleon <renleonz@gmail.com>
2022-07-05 09:39:35 -0500

    Create OrderListScreen

---
+++ frontend/src/screens/OrderListScreen.js
@@ -0,0 +1,94 @@
+import React, { useEffect } from "react";
+import { Button, Table } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { LinkContainer } from "react-router-bootstrap";
+import { useNavigate } from "react-router-dom";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { listOrders } from "../store/actions/orderActions";
+
+export const OrderListScreen = () => {
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
+  const orderList = useSelector((state) => state.orderList);
+  const { loading, error, orders } = orderList;
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  useEffect(() => {
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listOrders());
+      return;
+    }
+
+    navigate("/login");
+  }, [userInfo, dispatch, navigate, listOrders]);
+
+  const deleteHandler = (userId) => {
+    const message = "Are you sure to delete this user?";
+
+    if (window.confirm(message)) {
+      dispatch(deleteUser(userId));
+    }
+  };
+
+  return (
+    <div>
+      <h1>Orders</h1>
+
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Table stripped={"true"} bordered hover responsive className="table-sm">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>USER</th>
+              <th>DATE</th>
+              <th>PAID</th>
+              <th>DELIVERED</th>
+              <th></th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {orders.map((order) => (
+              <tr key={order._id}>
+                <td>{order._id}</td>
+                <td>{order.user && order.user.name}</td>
+                <td>{order.createdAt.substring(0, 10)}</td>
+                <td>{order.totalPrice}</td>
+                <td>
+                  {order.isPaid ? (
+                    order.paidAt.substring(0, 10)
+                  ) : (
+                    <i className="fas fa-times" style={{ color: "#f22" }}></i>
+                  )}
+                </td>
+                <td>
+                  {order.isDelivered ? (
+                    order.deliveredAt.substring(0, 10)
+                  ) : (
+                    <i className="fas fa-times" style={{ color: "#f22" }}></i>
+                  )}
+                </td>
+                <td>
+                  <LinkContainer to={`/order/${order._id}/`}>
+                    <Button variant="ligth" className="btn-sm">
+                      Details
+                    </Button>
+                  </LinkContainer>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </div>
+  );
+};
