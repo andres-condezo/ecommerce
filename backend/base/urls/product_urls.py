63541ae6ac9536b6aeb7ce0b3476720619ba7e01
rennleon <renleonz@gmail.com>
2022-07-09 15:32:08 -0500

    Create endpoint for product review

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
   path('create/', views.createProduct, name='product-create'),
   path('upload-image/', views.uploadImage, name='image-upload'),
   
+  path('<str:pk>/reviews/', views.createProductReview, name='create-review'),
+
   path('<str:pk>/', views.getProduct, name='product'),
   path('update/<str:pk>/', views.updateProduct, name='product-update'),
   path('delete/<str:pk>/', views.deleteProduct, name='product-delete'),
