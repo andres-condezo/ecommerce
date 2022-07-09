deeab0c2f1bfdc583fca7d2a5e1b79b0f5f0d923
rennleon <renleonz@gmail.com>
2022-07-09 15:28:08 -0500

    Craete view for product review

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -1,9 +1,11 @@
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 from rest_framework.response import Response
 
-from base.models import Product
+from rest_framework import status
+
+from base.models import Product, Review
 from base.serializers import (ProductSerializer)
 
 
@@ -76,3 +78,44 @@ def uploadImage(request):
   product.save()
   
   return Response('Image was uploaded')
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def createProductReview(request, pk):
+  user = request.user
+
+  product = Product.objects.get(_id=pk)
+  data = request.data
+
+  #1 - Review already exists
+  alreadyExists = product.review_set.filter(user=user).exists()
+  if alreadyExists:
+    content = {'details': 'Product already reviewed'}
+    return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+  #2 - Norating or 0
+  if data['rating'] == 0:
+    content = {'details': 'Please select a rating'}
+    return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+  #3 - Create review
+  review = Review.objects.create(
+    user=user,
+    product=product,
+    name=user.first_name,
+    rating=data['rating'],
+    comment=data['comment']
+  )
+
+  reviews = product.reviews_set.all()
+  product.numReviews = len(reviews)
+
+  total = 0
+  for review in reviews:
+    total += review.rating
+
+  product.rating = total / len(reviews)
+  product.save()
+
+  return Response('Review added')
