95e8ec4f5edaff317c513ef409b64f1f889c84c2
rennleon <renleonz@gmail.com>
2022-07-09 15:35:18 -0500

    Create new serializer for review

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -2,7 +2,7 @@ from copyreg import constructor
 from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.models import User
-from .models import Product, Order, OrderItem, ShippingAddress
+from .models import Product, Order, OrderItem, Review, ShippingAddress
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -42,10 +42,23 @@ class UserSerializerWithToken(UserSerializer):
         return str(token.access_token)
 
 
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
 class ProductSerializer(serializers.ModelSerializer):
+    reviews = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Product
         fields = '__all__'
+
+    def get_reviews(self, obj):
+        reviews = obj.review_set.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return serializer.data
 
 
 class ShippingAddressSerializer(serializers.ModelSerializer):
