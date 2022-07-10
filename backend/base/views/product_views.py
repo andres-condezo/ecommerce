2c71ce86d3eeaebeae7272787699efe70319dcb5
rennleon <renleonz@gmail.com>
2022-07-10 12:22:08 -0500

    Update key on error response 'details' to 'detail'

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -91,12 +91,12 @@ def createProductReview(request, pk):
   #1 - Review already exists
   alreadyExists = product.review_set.filter(user=user).exists()
   if alreadyExists:
-    content = {'details': 'Product already reviewed'}
+    content = {'detail': 'Product already reviewed'}
     return Response(content, status=status.HTTP_400_BAD_REQUEST)
 
   #2 - Norating or 0
   if data['rating'] == 0:
-    content = {'details': 'Please select a rating'}
+    content = {'detail': 'Please select a rating'}
     return Response(content, status=status.HTTP_400_BAD_REQUEST)
 
   #3 - Create review
