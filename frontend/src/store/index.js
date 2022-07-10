67398a0c6849e74de0a47806947fcd3fe539b460
rennleon <renleonz@gmail.com>
2022-07-10 11:44:42 -0500

    Add product review create reducer to reducers

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -7,6 +7,7 @@ import {
   productDeleteReducer,
   productDetailsReducers,
   productListReducers,
+  productReviewCreateReducer,
   productUpdateReducer,
 } from "./reducers/productReducers";
 
@@ -35,6 +36,7 @@ const reducer = combineReducers({
   productDelete: productDeleteReducer,
   productCreate: productCreateReducer,
   productUpdate: productUpdateReducer,
+  productReviewCreate: productReviewCreateReducer,
 
   cart: cartReducers,
 
