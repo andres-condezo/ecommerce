d966d0c246f6d56f05ae8de90d9c408b82a00eb5
rennleon <renleonz@gmail.com>
2022-07-10 12:55:52 -0500

    Updatete product list action to handle query params

--- frontend/src/store/actions/productActions.js
+++ frontend/src/store/actions/productActions.js
@@ -20,22 +20,24 @@ import {
 } from "../constants/productConstants";
 import axios from "axios";
 
-export const listProducts = () => async (dispatch) => {
-  try {
-    dispatch({ type: PRODUCT_LIST_REQUEST });
+export const listProducts =
+  (queryParams = "") =>
+  async (dispatch) => {
+    try {
+      dispatch({ type: PRODUCT_LIST_REQUEST });
 
-    const { data } = await axios.get("/api/products/");
+      const { data } = await axios.get(`/api/products/${queryParams}`);
 
-    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
-  } catch (error) {
-    dispatch({
-      type: PRODUCT_LIST_FAIL,
-      payload: error.response.data.message
-        ? error.response.data.message
-        : error.message,
-    });
-  }
-};
+      dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+    } catch (error) {
+      dispatch({
+        type: PRODUCT_LIST_FAIL,
+        payload: error.response.data.message
+          ? error.response.data.message
+          : error.message,
+      });
+    }
+  };
 
 export const listProductDetails = (id) => async (dispatch) => {
   try {
