bd888caf3408e22fc63a7e35b38f2afdb2e8481e
rennleon <renleonz@gmail.com>
2022-07-11 12:29:04 -0500

    Create Paginate component

---
+++ frontend/src/components/Paginate.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Pagination } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+export const Paginate = ({ pages, page, keyword = "", isAdmin = false }) => {
+  return (
+    pages > 1 && (
+      <Pagination>
+        {[...Array(pages).keys()].map((x) => {
+          <LinkContainer key={x + 1} to={`/keyword=${keyword}&page=${x + 1}`}>
+            <Pagination.Item active={x + 1 === page}>{x + 1}</Pagination.Item>
+          </LinkContainer>;
+        })}
+      </Pagination>
+    )
+  );
+};
