affb0bc340a8ab8846d765e8865b5a2cc57878ca
rennleon <renleonz@gmail.com>
2022-07-14 08:09:53 -0500

    Create endpoint for top products

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
   
   path('<str:pk>/reviews/', views.createProductReview, name='create-review'),
 
+  path('top/', views.getTopProducts, name='top-products'),
+
   path('<str:pk>/', views.getProduct, name='product'),
   path('update/<str:pk>/', views.updateProduct, name='product-update'),
   path('delete/<str:pk>/', views.deleteProduct, name='product-delete'),
