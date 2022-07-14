037c6a2ba7ec39ff886b56f1a6995ca61774c6e0
rennleon <renleonz@gmail.com>
2022-07-14 08:49:26 -0500

    Update component and add useEffect missing dependencies

---
+++ frontend/src/components/ProductCarousel.js
@@ -0,0 +1,39 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { listTopProducts } from "../store/actions/productActions";
+import { Carousel, Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Loader from "./Loader";
+import Message from "./Message";
+
+export const ProductCarousel = () => {
+  const dispatch = useDispatch();
+
+  const productTopRated = useSelector((state) => state.productTopRated);
+  const { error, loading, products } = productTopRated;
+
+  useEffect(() => {
+    dispatch(listTopProducts());
+  }, [dispatch]);
+
+  return loading ? (
+    <Loader />
+  ) : error ? (
+    <Message variant="danger">{error}</Message>
+  ) : (
+    <Carousel pause="hover" className="bg-dark">
+      {products.map((product) => (
+        <Carousel.Item key={product._id}>
+          <Link to={`/product/${product._id}`}>
+            <Image src={product.image} alt={product.name} fluid />
+            <Carousel.Caption className="carousel-caption">
+              <h4>
+                {product.name} (${product.price})
+              </h4>
+            </Carousel.Caption>
+          </Link>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+};
