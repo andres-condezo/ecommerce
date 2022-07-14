5caa5bf9f91d3796f72a946b7c03ac24174dbc39
rennleon <renleonz@gmail.com>
2022-07-14 08:08:36 -0500

    Create view to get top products based on rating

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -44,6 +44,13 @@ def getProducts(request):
 
 
 @api_view(['GET'])
+def getTopProducts(request):
+  products = Product.objects.filter(rating_gt=4).order_by('-rating')[0:5]
+  serializer =ProductSerializer(products, many=True)
+  return Response(serializer.data)
+
+
+@api_view(['GET'])
 def getProduct(request, pk):
   product = Product.objects.get(_id=pk)
   serializer = ProductSerializer(product, many=False)
